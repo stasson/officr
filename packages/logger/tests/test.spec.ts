659bb86f71befa88ba2cbfa187fce1fe8e46f1df
stasson <sebastien.tasson@gmail.com>
2022-01-06 21:12:26 +0100

    feat(logger): add logger package

--- packages/logger/tests/test.spec.ts
+++ packages/logger/tests/test.spec.ts
@@ -1,5 +1,27 @@
-describe('jest', () => {
-  it('can run unit tests', () => {
-    expect(1 + 1).toEqual(2)
+import logger from '../src'
+
+describe('logger', () => {
+  it('can log', () => {
+    const restoreCode = process.exitCode
+    logger.config({ stats: true, exitCode: true, timestamp: true })
+    logger.record('log-spec.log')
+    logger.debug('log')
+    logger.label('label').log('log')
+    logger.info('info')
+    logger.warn('warning')
+    logger.error('error')
+    logger.success('success')
+
+    expect(logger.stats).toMatchInlineSnapshot(`
+      Object {
+        "errors": 1,
+        "success": 1,
+        "total": 3,
+        "warnings": 1,
+      }
+    `)
+    logger.end()
+    expect(process.exitCode).toMatchInlineSnapshot(`-1`)
+    process.exitCode = restoreCode
   })
 })
